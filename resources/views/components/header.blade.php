4a5957acdcbaf17d450ec294e54671b0df053772
Amrit Rijal <rijalamrit720@gmail.com>
2024-10-01 20:08:17 +0545

    added some components

---
+++ resources/views/components/header.blade.php
@@ -0,0 +1,6 @@
+<header>
+   @php
+      $message = 'This is a dynamic alert message!';
+   @endphp
+   <x-navbar :example="$message" />
+</header>
---
+++ resources/views/components/navbar.blade.php
@@ -0,0 +1,11 @@
+<nav class="navbar">
+    <div class="container">
+        <a href="#" class="navbar-brand">Amrit Rijal</a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="navbarContent">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarContent">
+
+        </div>
+    </div>
+</nav>
